1a67451e4c71181a5d42a03a9d67400a2d4147eb
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-11-28 19:11:07 +0000

    Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,47 +9,17 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+
 size_t size, leaves;
 
 if (tree == NULL)
 return (0);
 
-size = binary_size_size(tree);
-leaves = binary_size_leaves(tree);
+size = binary_tree_size(tree);
+leaves = binary_tree_leaves(tree);
 
 return (size - leaves);
+
 }
 
 
-/**
-* binary_tree_size - measures the size of a binary tree
-* @tree: tree to measure the size of
-*
-* Return: size of the tree
-*         0 if tree is NULL
-*/
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-if (!tree)
-return (0);
-
-return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}
-
-/**
-* binary_tree_leaves - counts the leaves in a binary tree
-* @tree: tree to count the leaves from
-*
-* Return: number of leaves
-*         0 if tree is NULL
-*/
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-if (!tree)
-return (0);
-
-if (!tree->left && !tree->right)
-return (1);
-
-return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-}
