ccd5e5d728550b460cb36761b69b9ed526a63190
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-11-28 21:16:22 +0000

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -28,8 +28,8 @@ if (tree == NULL)
 return (1);
 
 if (((tree->left && tree->right) || (!tree->left && !tree->right)) &&
-binary_tree_is_full(tree->left) == 1 &&
-binary_tree_is_full(tree->right) == 1)
+is_full(tree->left) == 1 &&
+is_full(tree->right) == 1)
 return (1);
 
 return (0);
